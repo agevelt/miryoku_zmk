2987184bb9d36c6f1d99a0dba0f16e48909089f3
Audun Gevelt <a.gevelt@gmail.com>
2023-10-01 22:12:28 +0200

    fix rgb blinding

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -63,4 +63,13 @@ XXX  XXX  XXX  XXX  XXX            K32  K33  K34                 K35  K36  K37  
  &kp LSHFT K20  K21  K22  K23  K24                                     XXX           &kp PG_DN                                K25      K26     K27      K28    K29   &kp RSHFT \
      &none &kp GRAVE &kp CAPS &kp LEFT &kp RIGHT             K32  K33  K34           K35       K36     K37                        &kp DOWN  &kp UP &kp LBKT &kp RBKT &none
 
+
+
+// fix rgb keys, they shouldn't blind you
+#define MIRYOKU_LAYER_MEDIA \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_TOG,         U_RGB_EFF,         U_RGB_HUI,         &rgb_ug RGB_OFF,   &rgb_ug RGB_BRD,         \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
+U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
+
 #endif
